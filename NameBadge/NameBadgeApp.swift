d617a34629074a55eac82c4da5f2a1ae69213eda
Graeme Armstrong <garmstrong@lcs.on.ca>
2022-11-03 08:37:50 -0400

    Initial Commit

---
+++ NameBadge/NameBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameBadgeApp.swift
+//  NameBadge
+//
+//  Created by Graeme Armstrong on 2022-11-03.
+//
+
+import SwiftUI
+
+@main
+struct NameBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
