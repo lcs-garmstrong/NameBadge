125bb57f0a417d91996f9c2d341da0307a3713ae
Graeme Armstrong <garmstrong@lcs.on.ca>
2022-11-03 08:57:05 -0400

    Added a stored property to ContentView not keep track of our name.

--- NameBadge/ContentView.swift
+++ NameBadge/ContentView.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    
+    let myName: String
+    
+    
+    //MARK: Computed preoperties
+    
+    // Describess the user interface
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(myName)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(myName: "Mr. Armstrong")
     }
 }
